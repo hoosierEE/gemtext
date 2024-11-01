78a3ea0cef977cbebfeb7e8034c61a4210873a7a
Alex Shroyer <ashroyer@gmail.com>
2024-11-01 14:48:07 -0400

    update runner.sh to use static binary

--- runner.sh
+++ runner.sh
@@ -1,11 +1,16 @@
 #!/bin/zsh
 
-# generate html from reference implementation (uncomment next line to regenerate tests):
-# for f in tests/*.gmi; do go run g2h.go < $f > "${f/.gmi/.html}"; done
+# build reference implementation binary called "gemtext"
+go build .
+
+# generate html from reference implementation
+for f in tests/*.gmi; do
+    ./gemtext < $f > "${f/.gmi/.html}"
+done
 
 # compare with expected html
 for f in tests/*.gmi; do
-    if ! diff -q <(go run g2h.go < $f) "${f/.gmi/.html}"; then
+    if ! diff -q <(./gemtext < $f) "${f/.gmi/.html}"; then
         echo "exiting after first test failure"
         exit 1
     fi
